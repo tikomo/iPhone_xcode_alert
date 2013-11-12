1a639af701850d8b5ca655f973e3b90e1e999124
Komorita Tsuyoshi <tikomo@Komorita-no-MacBook-Air.local>
2013-11-12 14:50:23 +0900

    最初のコミット

--- alert/ViewController.h
+++ alert/ViewController.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+- (IBAction)buttonAlert_click:(id)sender;
 
 @end
--- alert/ViewController.m
+++ alert/ViewController.m
@@ -26,4 +26,42 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)buttonAlert_click:(id)sender {
+    
+    NSString * strTitle = @"Title";
+    //NSString * strTitle2 = [[NSString alloc] initWithString:@"Hello, World."];
+    
+    // アラートビューを作成
+    // キャンセルボタンを表示しない場合はcancelButtonTitleにnilを指定
+    UIAlertView *alert = [[UIAlertView alloc]
+                          initWithTitle:strTitle
+                          message:@"実行しますか？"
+                          delegate:self
+                          cancelButtonTitle:@"Cancel"
+                          otherButtonTitles:@"Button1", @"Button2", nil];
+    
+    // アラートビューを表示
+    [alert show];
+    
+}
+
+// delegate self
+// Alert ダイアログのボタンが押されたとき
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    switch (buttonIndex) {
+        case 1: // Button1が押されたとき
+            NSLog(@"Button1");
+            break;
+            
+        case 2: // Button2が押されたとき
+            NSLog(@"Button2");
+            break;
+            
+        default: // キャンセルが押されたとき
+            NSLog(@"Cancel");
+            break;
+    }
+}
+
 @end
