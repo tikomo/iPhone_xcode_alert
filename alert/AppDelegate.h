77b8cfd25403d98ab379ff878191c11e7fa39ec1
Komorita Tsuyoshi <tikomo@Komorita-no-MacBook-Air.local>
2013-11-12 14:45:47 +0900

    Initial Commit

---
+++ alert/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  alert
+//
+//  Created by Komorita Tsuyoshi on 2013/11/12.
+//  Copyright (c) 2013年 tikomo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
